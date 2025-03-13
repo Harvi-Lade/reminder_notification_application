de19673360de99266379886c4c4b2dacb5e64878
Harvi-Lade <72801210+Harvi-Lade@users.noreply.github.com>
2025-03-13 20:41:31 +0530

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,6 @@
+# Configuration settings DB path, email, API keys
 
+DB_NAME = "reminders.db"  # Name of the SQLite database file
+PUSHBULLET_API_KEY = ""  # Add your Pushbullet API Key
+EMAIL_SENDER = ""              # Email sender for notifications
+EMAIL_PASSWORD = ""           # Password for the sender's email
